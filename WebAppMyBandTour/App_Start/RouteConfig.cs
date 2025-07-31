333c7223760174acea562d8337443060541dbb44
mocampo2607 <mocampo2609@icloud.com>
2025-07-30 20:23:24 -0600

    Creación del controller y vistas.

--- WebAppMyBandTour/App_Start/RouteConfig.cs
+++ WebAppMyBandTour/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebAppMyBandTour
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Empresa", action = "Inicio", id = UrlParameter.Optional }
             );
         }
     }
